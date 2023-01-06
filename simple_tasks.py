4e41a36537cc624707ee3dfc1569e692e52ee256
Ramtin Z. Marandi <46963585+ramtinz@users.noreply.github.com>
2023-01-06 15:33:26 +0100

    Update simple_tasks.py

--- simple_tasks.py
+++ simple_tasks.py
@@ -1,5 +1,7 @@
 # Here is a simple Python code that performs some basic Linux administrative tasks:
 
+# This code defines several functions that can be used to perform various tasks, such as listing the files in the current directory, creating a new file, deleting a file, setting file permissions, and changing the owner of a file.
+
 import subprocess
 
 def list_files():
@@ -21,3 +23,6 @@ def set_permissions(filename, permissions):
 def change_owner(filename, owner):
   # Change the owner of the file with the given name to the specified user
   subprocess.run(["chown", owner, filename])
+
+  
+  # To use these functions, you would simply call them with the appropriate arguments. For example, to list the files in the current directory, you could call list_files(). To create a new file called myfile.txt, you could call create_file("myfile.txt").
